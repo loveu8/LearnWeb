c1929e8f6c9a010b3a32f2b9a4c5bcd94155c3a5
SL-ASUSROGGM501\StarQk <qkpigstar@gmail.com>
2025-11-12 20:38:45 +1000

    javaScript switch

---
+++ javascript/switch.js
@@ -0,0 +1,45 @@
+const day = 2;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("TUESDAY");
+        break;
+    case 3:
+        console.log("WEDNESDAY");
+        break;
+    case 4:
+        console.log("THURDAY");
+        break;
+    case 5:
+        console.log("FRIDAY");
+        break;
+    case 6:
+        console.log("SATURDAY");
+        break;
+    case 7:
+        console.log("SUNDAY");
+        break;
+    default:
+        console.log("I don't know");
+}
+
+// if (day === 1) {
+//     console.log("Monday");
+// } else if (day === 2) {
+//     console.log("TUESDAY");
+// } else if (day === 3) {
+//     console.log("WEDNESDAY");
+// } else if (day === 4) {
+//     console.log("THURDAY");
+// } else if (day === 5) {
+//     console.log("FRIDAY");
+// } else if (day === 6) {
+//     console.log("SATURDAY");
+// } else if (day === 7) {
+//     console.log("SUNDAY");
+// } else {
+//     console.log("I don't know");
+// }
